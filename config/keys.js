36d1cea5082c93d6aa77098cb72fecbf5647a970
Pawel Szczepkowski <pawel.szczepkowski@ttc.com>
2019-01-08 22:58:53 +0000

    google passport config

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  googleClientID: '658775791640-vqaulc0advuc5bm17iefhqj1m4b90tek.apps.googleusercontent.com',
+  googleClientSecret: 'BcZGo3FPq_LrVzN65xVxVt5k',
+}
--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const keys = require('./config/keys')
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({hi: "there"})
-})
+passport.use(new GoogleStrategy({
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSecret,
+  callbackURL: '/auth/google/callback'
+}, (accessToken) => {
+  console.log(accessToken)
+}))
 
+app.get('/auth/google', passport.authenticate('google', {
+  scope: ['profile', 'email']
+}))
 
+app.get('/auth/google/callback', passport.authenticate('google') )
 
 var port = process.env.PORT || 3000;
 app.listen(port, "0.0.0.0")

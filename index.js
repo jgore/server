5b1983d0554e29f6287f1546629b9a74a3ac84f9
Pawel Szczepkowski <pawel.szczepkowski@ttc.com>
2019-01-08 21:56:38 +0000

    PORT 3000

--- index.js
+++ index.js
@@ -5,5 +5,7 @@ app.get('/', (req, res) => {
   res.send({hi: "there"})
 })
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+
+
+var port = process.env.PORT || 3000;
+app.listen(port, "0.0.0.0")

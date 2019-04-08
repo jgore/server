f6d9313ac2e92e2666fdaa51f01e858a5465bb4d
codemasternode <marcinwarzybok@outlook.com>
2019-04-08 13:38:22 +0200

    basic routing

---
+++ client/src/components/Nav.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Link } from "react-router-dom"
+
+class Nav extends Component {
+    render() {
+        return (
+            <div>
+                UL
+            </div>
+        )
+    }
+}
+
+export default Nav
---
+++ client/src/pages/Account.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Account extends Component {
+    render() {
+        return (
+            <div className="Account">
+                Account
+      </div>
+        );
+    }
+}
+
+export default Account;

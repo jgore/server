36d7d984eecad5687e9ea3c8babc63b51fbdd902
Pawel Szczepkowski <pawel.szczepkowski@ttc.com>
2019-04-10 11:39:32 +0100

    1) edit contact .. add image

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import EmailForm from './../components/EmailForm'
-import {Jumbotron} from 'react-bootstrap'
+import {Jumbotron, Image} from 'react-bootstrap'
 import {PUBLIC_RESOURSES_URL} from "../utils/variables";
 
 class Contact extends Component {
@@ -11,22 +11,22 @@ class Contact extends Component {
       <div style={{marginTop: 20}} className="Contact">
         <Jumbotron>
           <div className="media">
-            <img className="img-thumbnail" src={`${PUBLIC_RESOURSES_URL}/lesz.jpg`} alt="Gjava building" />
-              <div style={{margin: 20}} className="media-body">
-                <h5  className="mt-0">Gjava - Szkolimy Programistów</h5>
-                <p>Leszczyńskiego 4, 50-078 <br />
-                  50-078, Wrocław
-                </p>
-                <p>Tel : 535 106 204</p>
-              </div>
+            <Image className="img-thumbnail" src={`${PUBLIC_RESOURSES_URL}/lesz.jpg`} alt="Gjava building"/>
+            <div style={{margin: 20}} className="media-body">
+              <h5 className="mt-0">Gjava - Szkolimy Programistów</h5>
+              <p>Leszczyńskiego 4, 50-078 <br/>
+                50-078, Wrocław
+              </p>
+              <p>Tel : 535 106 204</p>
+            </div>
           </div>
 
         </Jumbotron>
 
         <EmailForm/>
       </div>
-  );
+    );
   }
-  }
+}
 
-  export default Contact;
+export default Contact;

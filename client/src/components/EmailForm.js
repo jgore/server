0482a6aa4f534cfd715008d5d9bb59f57cbe541a
codemasternode <marcinwarzybok@outlook.com>
2019-04-10 20:00:53 +0200

    captcha working

--- client/src/components/EmailForm.js
+++ client/src/components/EmailForm.js
@@ -1,6 +1,8 @@
-import React, {Component} from 'react'
-import {API_URL} from "../utils/variables";
+import React, { Component } from 'react'
+import { API_URL } from "../utils/variables";
 import Axios from "axios/index";
+import ReCaptcha from 'react-recaptcha'
+import { Alert } from 'react-bootstrap'
 
 class EmailForm extends Component {
 
@@ -12,16 +14,31 @@ class EmailForm extends Component {
       emailed: false,
       courses: [],
       isLoading: true,
-      error: null
+      error: null,
+      isVerified: null
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleCaptcha = this.handleCaptcha.bind(this)
   }
+
+  handleCaptcha(response) {
+    if (response) {
+      this.setState({
+        isVerified: "VERIFIED"
+      })
+    } else {
+      this.setState({
+        isVerified: "UNVERIFIED"
+      })
+    }
+  }
+
 
   render() {
     return (
-      <div style={{marginTop: 50, marginBottom: 50}} className="border border-primary">
+      <div style={{ marginTop: 50, marginBottom: 50 }} className="border border-primary">
 
         <form onSubmit={this.handleSubmit}>
 
@@ -38,91 +55,118 @@ class EmailForm extends Component {
             </select>
           </div>
 
-          <div style={{ margin:20}} className="form-group">
+          <div style={{ margin: 20 }} className="form-group">
             <label htmlFor="text">Treść</label>
-            <textarea  required value={this.state.text} onChange={this.handleChange} cols={40} rows={10}
-                   className="form-control" id="text"
-                   placeholder="text"/>
+            <textarea required value={this.state.text} onChange={this.handleChange} cols={40} rows={10}
+              className="form-control" id="text"
+              placeholder="text" />
           </div>
 
-          <div style={{margin: 20}} className="form-group">
+          <div style={{ margin: 20 }} className="form-group">
             <label htmlFor="email">Email address</label>
             <input required value={this.state.email} onChange={this.handleChange} type="email" className="form-control"
-                   id="email"
-                   aria-describedby="emailHelp"
-                   placeholder="Enter email"/>
+              id="email"
+              aria-describedby="emailHelp"
+              placeholder="Enter email" />
             <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.
             </small>
           </div>
 
-          <div style={{margin: 20}} className="form-group">
+          <div style={{ margin: 20 }} className="form-group">
             <label htmlFor="phone">Phone number </label>
             <input value={this.state.phone} onChange={this.handleChange} type="phone" className="form-control"
-                   id="phone"
-                   aria-describedby="emailHelp"
-                   placeholder="Enter phone number"/>
+              id="phone"
+              aria-describedby="emailHelp"
+              placeholder="Enter phone number" />
             <small id="emailHelp" className="form-text text-muted">We will call you back if you will leave phone number.
             </small>
           </div>
-
-          <button style={{margin:20}} type="submit" className="btn btn-primary">Wyślij</button>
+          <ReCaptcha
+            sitekey="6LcsdZ0UAAAAAITDsNQyr6m-aGF5fDbF7O0AbByG"
+            render="explicit"
+            onloadCallback={this.recaptchaLoaded.bind(this)}
+            verifyCallback={this.handleCaptcha}
+          />
+          {
+            this.state.isVerified === null ?
+              "" :
+              this.state.isVerified ?
+                "" :
+                <Alert variant="danger" style={{ maxWidth: 400, marginTop: "1rem" }}>
+                  Proszę wypełnić Captche !!!
+                </Alert>
+          }
+          <button style={{ margin: 20 }} type="submit" className="btn btn-primary">Wyślij</button>
         </form>
       </div>
 
     )
   }
 
+
+
+  recaptchaLoaded() {
+    console.log("Captcha loaded")
+  }
+
   handleChange(event) {
     if (event.target.type === "text") {
-      this.setState({phone: event.target.value})
+      this.setState({ phone: event.target.value })
     }
     if (event.target.type === "email") {
-      this.setState({email: event.target.value})
+      this.setState({ email: event.target.value })
     }
     else if (event.target.type === "textarea") {
-      this.setState({text: event.target.value})
+      this.setState({ text: event.target.value })
     }
     else if (event.target.type === 'select-one') {
-      this.setState({subject: event.target.value});
+      this.setState({ subject: event.target.value });
     }
   }
 
   handleSubmit(event) {
-
-    Axios.post(`${API_URL}/api/sendEmail`, {
-      subject: this.state.subject,
-      text: this.state.text,
-      email: this.state.email,
-      phone : this.state.phone
-    })
-      .then((res) => {
-        alert('Email was sent')
-
-        this.setState({
-          emailed: res.data,
-          error: null
-        })
-      }).catch((err) => {
-      if (err.response) {
-        this.setState({
-          error: err.response.status
-        })
-        alert("Sorry - email was not sent because of server problems.")
-      } else {
-        this.setState({
-          error: 500
-        })
-      }
-    }).finally(() => {
-      this.setState({
-        isLoading: false,
-
-      })
-      window.location.reload()
-
-    })
     event.preventDefault();
+    console.log(this.state.isVerified)
+    if (this.state.isVerified) {
+      Axios.post(`${API_URL}/api/sendEmail`, {
+        subject: this.state.subject,
+        text: this.state.text,
+        email: this.state.email,
+        phone: this.state.phone
+      })
+        .then((res) => {
+          alert('Email was sent')
+
+          this.setState({
+            emailed: res.data,
+            error: null
+          })
+        }).catch((err) => {
+          if (err.response) {
+            this.setState({
+              error: err.response.status
+            })
+            alert("Sorry - email was not sent because of server problems.")
+          } else {
+            this.setState({
+              error: 500
+            })
+          }
+        }).finally(() => {
+          this.setState({
+            isLoading: false,
+
+          })
+          window.location.reload()
+        })
+    } else {
+      this.setState({
+        isVerified: false
+      })
+    }
   }
+
+
 }
 
 export default EmailForm
--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,20 +1,19 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import EmailForm from './../components/EmailForm'
-import {Jumbotron, Image} from 'react-bootstrap'
-import {PUBLIC_RESOURSES_URL} from "../utils/variables";
+import { Jumbotron, Image } from 'react-bootstrap'
+import { PUBLIC_RESOURSES_URL } from "../utils/variables";
 
 class Contact extends Component {
-
   render() {
     return (
 
-      <div style={{marginTop: 20}} className="Contact">
+      <div style={{ marginTop: 20 }} className="Contact">
         <Jumbotron>
           <div className="media">
-            <Image className="img-thumbnail" src={`${PUBLIC_RESOURSES_URL}/lesz.jpg`} alt="Gjava building"/>
-            <div style={{margin: 20}} className="media-body">
+            <Image className="img-thumbnail" src={`${PUBLIC_RESOURSES_URL}/lesz.jpg`} alt="Gjava building" />
+            <div style={{ margin: 20 }} className="media-body">
               <h5 className="mt-0">Gjava - Szkolimy Programistów</h5>
-              <p>Leszczyńskiego 4 <br/>
+              <p>Leszczyńskiego 4 <br />
                 50-078, Wrocław
               </p>
               <p>Tel : 535 106 204</p>
@@ -23,7 +22,7 @@ class Contact extends Component {
 
         </Jumbotron>
 
-        <EmailForm/>
+        <EmailForm />
       </div>
     );
   }

ccb67ed75042d265702b4289a3546297f85c46bd
Pawel Szczepkowski <pawel.szczepkowski@ttc.com>
2019-04-10 07:42:55 +0100

    1) add building foto on contact 2) add java page text - design 3) edit data in db.json 4) Refactor main class usage - not sure if correct

--- client/src/components/EmailForm.js
+++ client/src/components/EmailForm.js
@@ -30,10 +30,10 @@ class EmailForm extends Component {
             <label className="mr-sm-2" htmlFor="subject">Temat</label>
             <select onChange={this.handleChange} className="custom-select mr-sm-2" id="subject">
               <option selected>Wybierz ...</option>
-              <option value="java1">Kurs Programowania Java 1 - Company [ 0 lat doświadczenia ]</option>
-              <option value="java2">Kurs Programowania Java 2 - Shop [ 0,1,2 lat doświadczenia ]</option>
-              <option value="java2++">Kurs Programowania Java 2++ - FlyWithUs [ rozmowa kwalifikacyjna +  zadanie ] - [ 1,2 lat doświadczenia ]</option>
-              <option value="java3">Kurs programowania Java 3 - Tropics 2 [ 2++ lat doświadczenia ]</option>
+              <option value="java0">Kurs Programowania Java 0 - Company [ 0 lat doświadczenia ]</option>
+              <option value="java1">Kurs Programowania Java 1 - Shop [ 0,1,2 lat doświadczenia ]</option>
+              <option value="java1++">Kurs Programowania Java 1++ - FlyWithUs [ projekt + rozmowa kwalifikacyjna + zadanie ] - [ 1,2 lat doświadczenia ]</option>
+              <option value="java2">Kurs programowania Java 2 - Tropics 2 [ 2++ lat doświadczenia ]</option>
               <option value="others"> Pozostałe</option>
             </select>
           </div>
--- client/src/pages/Java.js
+++ client/src/pages/Java.js
@@ -1,13 +1,43 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class Java extends Component {
-    render() {
-        return (
-            <div className="Java">
-                Java
-            </div>
-        );
-    }
+  render() {
+    return (
+
+      <div>
+
+        <div class="card" >
+          <img src="..." class="card-img-top" alt="..."/>
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">1. GJava - widelki finansowe - Junior, Mid, Senior</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+
+
+        <div class="card">
+          <img src="..." class="card-img-top" alt="..."/>
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text"> 2. Gjava - fundamentalna struktura warstwowa projektów w JAVA</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+
+        <div class="card">
+          <img src="..." class="card-img-top" alt="..."/>
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text"> 3. Gjava - Scrum i jego znaczenie w developmencie</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+
+      </div>
+    )
+
+  }
 }
 
 export default Java;

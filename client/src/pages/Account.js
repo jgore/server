55d522623c5866f7dd1c2d0fa39ec8c4f7db7f55
Pawel Szczepkowski <pawel.szczepkowski@ttc.com>
2019-04-10 14:13:19 +0100

    account - only for logged users

--- client/src/pages/Account.js
+++ client/src/pages/Account.js
@@ -1,16 +1,18 @@
-import React, { Component } from 'react';
-import { Jumbotron, Button } from 'react-bootstrap'
+import React, {Component} from 'react';
+import {Jumbotron, Button} from 'react-bootstrap'
 
 class Account extends Component {
-    render() {
-        return (
-            <div className="Account">
-                <Jumbotron>
-                Nie kupiłeś jeszcze żadnych produktów
-                </Jumbotron>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="Account">
+        <Jumbotron>
+          <button type="button" className="btn btn-secondary btn-lg btn-block"> Ta sekcja jest widoczna tylko dla
+            zalogowanych użytkowników
+          </button>
+        </Jumbotron>
+      </div>
+    );
+  }
 }
 
 export default Account;

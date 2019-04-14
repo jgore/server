872e06b344ec9cfac574ccae80627b6054ae2ab3
Pawel Szczepkowski <pawel.szczepkowski@ttc.com>
2019-04-14 14:27:04 +0100

    remove sens data
    (cherry picked from commit 9edd191)

--- client/src/components/EmailForm.js
+++ client/src/components/EmailForm.js
@@ -45,10 +45,10 @@ class EmailForm extends Component {
             <h2 className="header">Formularz kontaktowy</h2>
             <label className="mr-sm-2" htmlFor="subject">Temat</label>
             <select onChange={this.handleChange} className="custom-select mr-sm-2" id="subject">
-              <option selected>Wybierz ...</option>
+              <option selected>Wybierz kurs...</option>
               <option value="java0">Kurs Programowania Java 0 - Company [ 0 lat doświadczenia ]</option>
               <option value="java1">Kurs Programowania Java 1 - Shop [ 0,1,2 lat doświadczenia ]</option>
-              <option value="java1++">Kurs Programowania Java 1++ - FlyWithUs [ projekt + rozmowa kwalifikacyjna + zadanie ] - [ 1,2 lat doświadczenia ]</option>
+              <option value="java1++">Kurs Programowania Java 1++ - Flights [ projekt + rozmowa kwalifikacyjna + zadanie ] - [ 1,2 lat doświadczenia ]</option>
               <option value="java2">Kurs programowania Java 2 - Bookify 2 [ 2++ lat doświadczenia ]</option>
               <option value="others"> Pozostałe</option>
             </select>

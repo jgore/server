47bebf2471b6c04b822ad4ba3618775182aa4be0
Pawel Szczepkowski <pawel.szczepkowski@ttc.com>
2019-05-02 10:00:02 +0100

    fix after merge develop

--- models/User.js
+++ models/User.js
@@ -1,11 +1,17 @@
-const mongoose = require('mongoose')
-const {Schema} = mongoose
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
 const userSchema = new Schema({
   googleId: String,
   name: String,
   email: String,
-  phone: String
-})
+  phone: String,
+  image: {
+    type: String,
+    default: "guest.png"
+  }
+});
 
-mongoose.model('users', userSchema)
+mongoose.model("users", userSchema);
+
+module.exports = mongoose.model("users", userSchema);

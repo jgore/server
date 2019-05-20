8e6cb0a9251d485a62377e8b6e3368eb7ab86891
codemasternode <marcinwarzybok@outlook.com>
2019-05-20 13:36:51 +0200

    logging working

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fs = require("fs");
 const cors = require("cors");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
@@ -7,7 +8,7 @@ const keys = require("./config/keys");
 
 require("dotenv/config");
 const redisClient = require("./config/connectToCache");
-
+const logWriteStream = fs.createWriteStream("log.txt", { flags: "a+" });
 require("./models/User");
 require("./models/Message");
 require("./services/passport");
@@ -16,7 +17,31 @@ connectToDb(keys.mongoURI);
 const app = express();
 const bodyParser = require("body-parser");
 
+process.on("uncaughtException", function(err) {
+  console.log(err instanceof Error)
+  if (err instanceof Error === false) {
+    logWriteStream.write(
+      `\r\n[Error] ${JSON.stringify(err, undefined, 2)}\r\n`
+    );
+  } else {
+    logWriteStream.write(`\r\n[Error] ${err}\r\n`);
+  }
+
+  logWriteStream.end(
+    "\r\n -------------------------------END-------------------------------"
+  );
+});
+
+process.on("exit", function() {
+  logWriteStream.end();
+});
+
+
 app.use(cors());
+app.use((req, res, next) => {
+  logWriteStream.write(`\r\n[${req.method}] ${req.url}`);
+  next();
+});
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
@@ -30,7 +55,6 @@ app.use(
     keys: [keys.cookieKey]
   })
 );
-
 app.use(passport.initialize());
 app.use(passport.session());
 

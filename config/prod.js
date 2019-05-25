0318f1ba9b48382dbd453f8cd33d2655d14c856d
goreit <p.szczepkowski87@gmail.com>
2019-05-25 05:29:18 +0200

    hard code fix

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,8 @@
+
+//@FIXME @pszczepkowski .. fix on production google env variable
 module.exports = {
-  googleClientID: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  googleClientID: '658775791640-vqaulc0advuc5bm17iefhqj1m4b90tek.apps.googleusercontent.com',
+  googleClientSecret: 'BcZGo3FPq_LrVzN65xVxVt5k',
   cookieKey: process.env.COOKIE_KEY,
   mongoURI: process.env.MONGO_URI,
 }
